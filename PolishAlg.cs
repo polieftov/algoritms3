a77acd4cfb61e18e465843b7d8197f4c1e33d0f2
polieftov <polieftovegor@gmail.com>
2021-05-10 12:48:36 +0500

    initial commit

---
+++ PolishAlg.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabWork3
+{
+    class PolishAlg
+    {
+        private string brackets = "(){}[]";
+        private string openBr = "({[";
+        private string operations = "+-/*=%sincostanctglog";
+        public string formula;
+
+        public PolishAlg(string _formula)
+        {
+            formula = _formula;
+        }
+
+        private int priority (string op)
+        {
+            return op == "+" || op == "-" ? 1 :
+        op == "*" || op == "/" || op == "%"? 2 :
+        op == "="? 0 :
+        op == "sin"|| op == "cos" || op == "tan" || op == "ctg" || op =="log" || op == "exp"? 4 :
+        op == "^"? 3: -1;
+        }
+
+        public string Transform()
+        {
+            var pForm = SplitStr(formula);
+            var res = new Stack<string>();
+            var oper = new Stack<string>();
+            foreach(var c in pForm)
+            {
+                if (openBr.Contains(c))
+                {
+                    oper.Push(c);
+                }
+                else if (brackets.Contains(c))
+                {
+                    while(!openBr.Contains(oper.Peek()))
+                    {
+                        res.Push(oper.Pop());
+                    }
+                    oper.Pop();
+                }
+                else if (operations.Contains(c))
+                {
+
+                    while (oper.Count != 0 && priority(oper.Peek()) >= priority(c))
+                        res.Push(oper.Pop());
+                    oper.Push(c);
+                }
+                else
+                {
+                    res.Push(c);
+                }
+            }
+            while(oper.Count != 0)
+            {
+                res.Push(oper.Pop());
+            }
+            return ConvertToStr(res);
+        }
+
+        private string ConvertToStr(Stack<string> res)
+        {
+            var result = new StringBuilder();
+            var arr = res.ToArray();
+            for (var i = arr.Length-1; i >= 0; i--)
+            {
+                result.Append(arr[i].ToString());
+                if (i != 0)
+                    result.Append(",");
+            }
+            return result.ToString();
+        }
+
+        private string[] SplitStr(string formula)
+        {
+            var res = new List<string>();
+            for (var i = 0; i < formula.Length; i++)
+            {
+                if (Char.IsDigit(formula[i]) || (formula[i] == '-' && (i == 0 || openBr.Contains(formula[i - 1]) || operations.Contains(formula[i - 1])) && Char.IsDigit(formula[i+1])))
+                {
+                    var a = formula[i].ToString();
+                    if (formula.Length != i + 1)
+                    {
+
+
+                        while (Char.IsDigit(formula[i + 1]))
+                        {
+                            i++;
+                            a += formula[i].ToString();
+                            if (formula.Length <= i + 1)
+                                break;
+                        }
+                    }
+                    res.Add(a);
+                }
+                else
+                {
+                    if ((formula[i] == '-' && (i == 0 || openBr.Contains(formula[i - 1]) || operations.Contains(formula[i - 1]))) && !operations.Contains(formula[i + 1]))
+                    {
+                        res.Add(formula[i].ToString() + formula[i + 1].ToString());
+                        i++;
+                    }
+                    else if (i + 4 < formula.Length && operations.Contains(formula[i].ToString() + formula[i + 1].ToString() + formula[i + 2].ToString() + formula[i + 3].ToString()))
+                    {
+                        i += 4;
+                        res.Add(formula[i].ToString() + formula[i + 1].ToString() + formula[i + 2].ToString() + formula[i + 3].ToString());
+                    }
+                    else if (i + 3 < formula.Length && operations.Contains(formula[i].ToString() + formula[i + 1].ToString() + formula[i + 2].ToString()) && formula[i + 3] == '(')
+                    {
+                        
+                        //res.Add(formula[i].ToString() + formula[i + 1].ToString() + formula[i + 2].ToString());
+                        var j = i + 4;
+                        var s = "";
+                        while (formula[j] != ')')
+                        {
+                            s += formula[j].ToString();
+                            j++;
+                        }
+
+                        var microRes = new PolishAlg(s);
+                        res.Add(microRes.Transform() + "," + formula[i].ToString() + formula[i + 1].ToString() + formula[i + 2].ToString());
+                        i += j - i + 5;
+                    }
+                    else
+                    {
+                        res.Add(formula[i].ToString());
+                    }
+                }
+            }
+            return res.ToArray();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace LabWork3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var t1 = new PolishAlg("2*4");         
+            if (t1.Transform() != "2,4,*")
+                Console.WriteLine("Test 1 failed");
+
+            var t2 = new PolishAlg("x+5");
+            if (t2.Transform() != "x,5,+")
+                Console.WriteLine("Test 2 failed");
+
+            var t3 = new PolishAlg("x+5*2-3");
+            if (t3.Transform() != "x,5,2,*,+,3,-")
+                Console.WriteLine("Test 3 failed");
+
+            var t4 = new PolishAlg("x+5=32/2");
+            if (t4.Transform() != "x,5,+,32,2,/,=")
+                Console.WriteLine("Test 4 failed");
+
+            var t5 = new PolishAlg("(x+5)*(2-3)");
+            if (t5.Transform() != "x,5,+,2,3,-,*")
+                Console.WriteLine("Test 5 failed");
+
+            var t6 = new PolishAlg("-x+5");
+            if (t6.Transform() != "-x,5,+")
+                Console.WriteLine("Test 6 failed");
+
+            var t7 = new PolishAlg("sin(87)");
+            if (t7.Transform() != "87,sin")
+                Console.WriteLine("Test 7 failed");
+
+            var t8 = new PolishAlg("sin(87+x)");
+            if (t8.Transform() != "87,x,+,sin")
+                Console.WriteLine("Test 8 failed");
+
+            var t9 = new PolishAlg("cos(32*x/5)");
+            if (t9.Transform() != "32,x,*,5,/,cos")
+                Console.WriteLine("Test 9 failed");
+
+            var t10 = new PolishAlg("cos(32*-x/5)");
+            if (t10.Transform() != "32,-x,*,5,/,cos")
+                Console.WriteLine("Test 10 failed");
+
+            var t11 = new PolishAlg("tan(124-x/5)");
+            if (t11.Transform() != "124,x,5,/,-,tan")
+                Console.WriteLine("Test 11 failed");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
